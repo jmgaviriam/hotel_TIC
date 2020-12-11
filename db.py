3df54a4ddf633046b6a3a14e90c3dce26cc64f74
Jhoan Gaviria <jmgaviriam@gmail.com>
2020-12-11 13:03:17 -0500

    endpoints

--- db.py
+++ db.py
@@ -1,16 +1,21 @@
 from pydantic import BaseModel
 import datetime
+from typing import Dict
+
+
 
 class Reserva(BaseModel):
-    habitacion_id:int
-    usuario_id:str
-    fecha_inicio: datetime.date
-    fecha_fin: datetime.date
+    habitacion_id: str
+    usuario_id: str
+    fecha_inicio: str
+    fecha_fin: str
 
-reservas={
-    1:Reserva(habitacion_id=1,usuario_id=1,fecha_inicio='2020-12-24',fecha_fin='2021-01-14'),
-    2:Reserva(habitacion_id=2,usuario_id=2,fecha_inicio='2020-12-24',fecha_fin='2021-01-15')
 
+reservas: Dict[str, Reserva]
+
+reservas = { 
+    "1": Reserva(habitacion_id="1",usuario_id="1",fecha_inicio="2020-12-24",fecha_fin="2021-01-14"),
+    "2": Reserva(habitacion_id="2",usuario_id="2",fecha_inicio="2020-12-24",fecha_fin="2021-01-15"),
 }
 
 
@@ -21,7 +26,28 @@ def obtener_lista_reservas():
         lista_reservas.append(reservas[reserva])
         
     return lista_reservas
+
+
+
+def obtener_reserva(habitacion_id: int):
+    if habitacion_id in Reserva:
+        return Reserva(habitacion_id)
+    else:
+        return None
+
+def crear_reserva(reserva: Reserva):
+    if  reserva.habitacion_id in reservas:
+        return False
+    else:
+        reserva[reserva.habitacion_id] = reserva
+        return True
+
+def eliminar_reserva(reserva: Reserva):
+    if reserva.habitacion_id in reservas:
+        del reserva[reserva.habitacion_id] 
+        return True
+    else:
+        return False
+
     
 #if (reservas[reserva][fecha_inicio]>fecha_inicial) and (reservas[reserva][fecha_final]<fecha_final) :
-            
-
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI,HTTPException
 import db
 
-api = FastAPI()
+api=FastAPI()
 
 @api.get("/reservas")
 async def obtener_ocupacion():
     return db.obtener_lista_reservas()
+
+@api.post("/reservas/crear")
+async def crear_reserva(reserva: db.reservas):
+    reserva_exitosa = db.crear_reserva(reserva)
+    if reserva_exitosa:
+        return{"Mensaje": "Reservación creada con éxito"}
+    else:
+        raise HTTPException(status_code=400, detail="No se pudo realziar la reservación")
